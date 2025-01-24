bc4f660127d4f877ff7abf9982ea4b370676fc47
Kumod Sharma <kumod.aws@gmail.com>
2025-01-24 08:08:08 +0530

    1258-article-views-i.sql

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,25 +1,25 @@
 # Write your MySQL query statement below
 
 -- Method 1: Using Group By.
--- SELECT 
---     author_id AS id
--- FROM
---     views
--- WHERE
---     author_id = viewer_id
--- GROUP BY 1
--- HAVING COUNT(viewer_id) >= 1
--- ORDER BY 1;
+SELECT 
+    author_id AS id
+FROM
+    views
+WHERE
+    author_id = viewer_id
+GROUP BY 1
+HAVING COUNT(viewer_id) >= 1
+ORDER BY 1;
 
 
 -- -- Method 2: Using Conditions
--- SELECT DISTINCT
---     author_id AS id
--- FROM
---     views
--- WHERE
---     author_id = viewer_id
--- ORDER BY 1;
+SELECT DISTINCT
+    author_id AS id
+FROM
+    views
+WHERE
+    author_id = viewer_id
+ORDER BY 1;
 
 
 -- Method 3: Using Windows Function
@@ -31,10 +31,11 @@ FROM (
 ) sub
 WHERE self_view_count >= 1;
 
--- SELECT DISTINCT author_id AS id
--- FROM views v1
--- WHERE EXISTS (
---     SELECT 1
---     FROM views v2
---     WHERE v1.author_id = v2.author_id AND v2.author_id = v2.viewer_id
--- ) order by 1;
+-- Method 4: Using Exists
+SELECT DISTINCT author_id AS id
+FROM views v1
+WHERE EXISTS (
+    SELECT 1
+    FROM views v2
+    WHERE v1.author_id = v2.author_id AND v2.author_id = v2.viewer_id
+) order by 1;
