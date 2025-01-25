88c3d0ae58e6a294efd68713e053aa5972266080
Kumod Sharma <kumod.aws@gmail.com>
2025-01-25 09:53:16 +0530

    Time: 982 ms (48.02%), Space: 0B (100%) - LeetHub

---
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+-- Method 1: Using condition.
+-- SELECT 
+--     tweet_id
+-- FROM
+--     tweets
+-- WHERE
+--     LENGTH(content) > 15;
+
+
+-- Method 2: Using Not:
+SELECT 
+    tweet_id
+FROM
+    tweets
+WHERE
+    NOT LENGTH(content) <= 15;
