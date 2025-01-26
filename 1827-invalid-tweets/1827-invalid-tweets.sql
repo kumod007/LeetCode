316b7b77c713d69760930402b4d2d7fae61c2037
Kumod Sharma <kumod.aws@gmail.com>
2025-01-26 10:28:35 +0530

    1827-invalid-tweets.sql

--- 1827-invalid-tweets/1827-invalid-tweets.sql
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 
 -- Method 1: Using condition.
--- SELECT 
---     tweet_id
--- FROM
---     tweets
--- WHERE
---     LENGTH(content) > 15;
+SELECT 
+    tweet_id
+FROM
+    tweets
+WHERE
+    LENGTH(content) > 15;
 
 
 -- Method 2: Using Not:
