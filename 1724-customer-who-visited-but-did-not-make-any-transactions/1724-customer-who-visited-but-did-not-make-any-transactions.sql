a0ae7478b0cf2d3fb6065732ddd13acebf9c8b62
Kumod Sharma <kumod.aws@gmail.com>
2025-01-28 09:54:11 +0530

    1724-customer-who-visited-but-did-not-make-any-transactions.sql

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 
 -- Method 1: Using Left Join.
--- SELECT 
---     customer_id,
---     (COUNT(v.visit_id) - COUNT(transaction_id)) AS count_no_trans
--- FROM
---     visits AS v
---         LEFT JOIN
---     Transactions AS t ON v.visit_id = t.visit_id
--- GROUP BY customer_id
--- HAVING count_no_trans > 0;
+SELECT 
+    customer_id,
+    (COUNT(v.visit_id) - COUNT(transaction_id)) AS count_no_trans
+FROM
+    visits AS v
+        LEFT JOIN
+    Transactions AS t ON v.visit_id = t.visit_id
+GROUP BY customer_id
+HAVING count_no_trans > 0;
 
 
 -- Method 2: Using Subquery.
