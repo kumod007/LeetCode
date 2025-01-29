8e7eff07ea39baba67c8595c6791f0e728ae756e
Kumod Sharma <kumod.aws@gmail.com>
2025-01-29 07:34:47 +0530

    0197-rising-temperature.sql

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,14 +2,14 @@
 
 
 -- Method 1: Using Datediff and Condition.
--- SELECT 
---     w1.id AS Id
--- FROM
---     weather w1,
---     weather w2
--- WHERE
---     DATEDIFF(w1.recordDate, w2.recordDate) = 1
---         AND w1.temperature > w2.temperature;
+SELECT 
+    w1.id AS Id
+FROM
+    weather w1,
+    weather w2
+WHERE
+    DATEDIFF(w1.recordDate, w2.recordDate) = 1
+        AND w1.temperature > w2.temperature;
 
 
 
