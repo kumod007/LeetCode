a153428e4e6dffea2ebe39f9d02f4df88662bd73
Kumod Sharma <kumod.aws@gmail.com>
2025-01-31 06:09:13 +0530

    0577-employee-bonus.sql

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,26 +1,26 @@
 # Write your MySQL query statement below
 
 -- Method 1: Left Join and Condition.
--- SELECT 
---     e.name, b.bonus
--- FROM
---     employee e
---         LEFT JOIN
---     Bonus b USING (empid)
--- WHERE
---     b.bonus < 1000 OR b.bonus IS NULL;
+SELECT 
+    e.name, b.bonus
+FROM
+    employee e
+        LEFT JOIN
+    Bonus b USING (empid)
+WHERE
+    b.bonus < 1000 OR b.bonus IS NULL;
 
 
--- -- Method 2:
+-- -- Method 2: Using IFNULL and Left JOIN
 
--- SELECT 
---     Employee.name, Bonus.bonus
--- FROM
---     Employee
---         LEFT JOIN
---     Bonus USING (empId)
--- WHERE
---     IFNULL(Bonus.bonus, 0) < 1000;
+SELECT 
+    Employee.name, Bonus.bonus
+FROM
+    Employee
+        LEFT JOIN
+    Bonus USING (empId)
+WHERE
+    IFNULL(Bonus.bonus, 0) < 1000;
 
 
 -- Method 3: Without using Left Join.
