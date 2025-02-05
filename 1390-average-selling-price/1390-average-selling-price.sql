9630a95db8f26e1aa1c841c47b783935fd67b2cc
Kumod Sharma <kumod.aws@gmail.com>
2025-02-05 10:43:14 +0530

    1390-average-selling-price.sql

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,10 +1,33 @@
 # Write your MySQL query statement below. 
 
--- select product_id, case when average_price is null then 0 else average_price end as average_price from (
--- SELECT p.product_id, round((sum(p.price * u.units)/sum(u.units)),2) as average_price
--- FROM prices AS p LEFT JOIN UnitsSold AS u ON u.product_id = p.product_id 
--- AND u.purchase_date BETWEEN p.start_date AND p.end_date group by p.product_id) as table_1;
+-- Method 1:
+  SELECT 
+    product_id,
+    CASE
+        WHEN average_price IS NULL THEN 0
+        ELSE average_price
+    END AS average_price
+FROM
+    (SELECT 
+        p.product_id,
+            ROUND((SUM(p.price * u.units) / SUM(u.units)), 2) AS average_price
+    FROM
+        prices AS p
+    LEFT JOIN UnitsSold AS u ON u.product_id = p.product_id
+        AND u.purchase_date BETWEEN p.start_date AND p.end_date
+    GROUP BY p.product_id) AS table_1;
 
-SELECT p.product_id, IFNULL(round((sum(p.price * u.units)/sum(u.units)),2),0) as average_price
-FROM prices AS p LEFT JOIN UnitsSold AS u ON u.product_id = p.product_id 
-AND u.purchase_date BETWEEN p.start_date AND p.end_date group by p.product_id;
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Method 2:
+SELECT 
+    p.product_id,
+    IFNULL(ROUND((SUM(p.price * u.units) / SUM(u.units)),
+                    2),
+            0) AS average_price
+FROM
+    prices AS p
+        LEFT JOIN
+    UnitsSold AS u ON u.product_id = p.product_id
+        AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
