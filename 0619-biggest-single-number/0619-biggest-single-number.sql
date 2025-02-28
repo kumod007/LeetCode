3869310e1449d2d33f9c3fffaf1bffe9c44c8469
Kumod Sharma <kumod.aws@gmail.com>
2025-02-28 07:10:53 +0530

    Time: 899 ms (24.69%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,14 +1,18 @@
 # Write your MySQL query statement below
 
-SELECT COALESCE(num, NULL) AS num 
-FROM (
-    SELECT num 
-    FROM mynumbers 
-    GROUP BY num 
-    HAVING COUNT(*) = 1 
-    ORDER BY num DESC 
-    LIMIT 1
-) AS table_1
-UNION ALL
-SELECT NULL
-LIMIT 1;
+
+WITH
+  UniqueNumbers AS (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY 1
+    HAVING COUNT(num) = 1
+  )
+SELECT MAX(num) AS num
+FROM UniqueNumbers;
+
+
+-- SELECT COALESCE(num, NULL) AS num FROM (SELECT num FROM mynumbers GROUP BY num 
+--     HAVING COUNT(*) = 1 ORDER BY num DESC LIMIT 1) AS table_1
+-- UNION ALL
+-- SELECT NULL LIMIT 1;
