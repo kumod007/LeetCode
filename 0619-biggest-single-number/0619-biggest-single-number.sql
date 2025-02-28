e45996b4d73ba711b40f2490cd266cd3ab4f57f8
Kumod Sharma <kumod.aws@gmail.com>
2025-02-28 07:15:03 +0530

    Update 0619-biggest-single-number.sql

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,18 +1,30 @@
 # Write your MySQL query statement below
 
-
+----------------------
+# Method 1: Using CTE:
+----------------------
 WITH
   UniqueNumbers AS (
     SELECT num
     FROM MyNumbers
     GROUP BY 1
-    HAVING COUNT(num) = 1
-  )
+    HAVING COUNT(num) = 1)
 SELECT MAX(num) AS num
 FROM UniqueNumbers;
 
 
--- SELECT COALESCE(num, NULL) AS num FROM (SELECT num FROM mynumbers GROUP BY num 
---     HAVING COUNT(*) = 1 ORDER BY num DESC LIMIT 1) AS table_1
--- UNION ALL
--- SELECT NULL LIMIT 1;
+---------------------------
+# Method 2: Using COALESCE:
+---------------------------
+SELECT 
+    COALESCE(num, NULL) AS num
+FROM
+    (SELECT 
+        num
+    FROM
+        mynumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+    ORDER BY num DESC
+    LIMIT 1) AS table_1 
+UNION ALL SELECT NULL LIMIT 1;
